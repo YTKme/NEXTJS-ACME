826848fc1be3eecb77428509122a3ee75efc3840
Yan Kuang <YTKme@Outlook.com>
2024-08-28 15:35:03 -0700

    Update seed route

---
+++ alpha/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+/**
+ * Dashboard Page
+ */
+
+
+import type { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'NEXT.JS ACME | Dashboard',
+  description: 'NEXT.JS ACME | Dashboard',
+}
+
+export default function DashboardPage() {
+  return (
+    <p>Dashboard Page</p>
+  );
+}
