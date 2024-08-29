7a91bfa22e5125d11f9af12f8268186f095cb0c9
Yan Kuang <YTKme@Outlook.com>
2024-08-29 10:51:06 -0700

    Update alpha database seed

--- alpha/app/seed/route.ts
+++ alpha/app/seed/route.ts
@@ -18,33 +18,45 @@ const pool = new Pool({
 });
 
 async function seedUser() {
-  await pool.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS users (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL
-    );
-  `);
-
-  const insertedUsers = await Promise.all(
-    users.map(async (user) => {
-      const hashedPassword = await bcrypt.hash(user.password, 10);
-      return pool.query(
-        'INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO NOTHING;',
-        [user.id, user.name, user.email, hashedPassword],
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    await client.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
       );
-    }),
-  );
+    `);
 
-  return insertedUsers;
+    const insertedUsers = await Promise.all(
+      users.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        return client.query(
+          'INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO NOTHING;',
+          [user.id, user.name, user.email, hashedPassword],
+        );
+      }),
+    );
+    await client.query('COMMIT');
+
+    return insertedUsers;
+  } catch (error) {
+    await client.query('ROLLBACK')
+    throw error;
+  } finally {
+    client.release();
+  }
 }
 
 async function seedCustomer() {
-  await pool.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
-
-  await pool.query(`
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    await client.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
+    await client.query(`
     CREATE TABLE IF NOT EXISTS customers (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
@@ -53,21 +65,30 @@ async function seedCustomer() {
     );
   `);
 
-  const insertedCustomers = await Promise.all(
-    customers.map(
-      (customer) =>
-        pool.query(
-          'INSERT INTO customers (id, name, email, image_url) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO NOTHING;',
-          [customer.id, customer.name, customer.email, customer.image_url],
-        ),
-    ),
-  );
+    const insertedCustomers = await Promise.all(
+      customers.map(
+        (customer) =>
+          client.query(
+            'INSERT INTO customers (id, name, email, image_url) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO NOTHING;',
+            [customer.id, customer.name, customer.email, customer.image_url],
+          ),
+      ),
+    );
+    await client.query('COMMIT');
 
-  return insertedCustomers;
+    return insertedCustomers;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    throw error;
+  } finally {
+    client.release();
+  }
 }
 
 async function seedInvoice() {
-  await pool.query(`
+  const client = await pool.connect();
+  try {
+    await client.query(`
     CREATE TABLE IF NOT EXISTS invoices (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       customer_id UUID NOT NULL,
@@ -77,38 +98,52 @@ async function seedInvoice() {
     );
   `);
 
-  const insertedInvoices = await Promise.all(
-    invoices.map(
-      (invoice) =>
-        pool.query(
-          'INSERT INTO invoices (customer_id, amount, status, date) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO NOTHING;',
-          [invoice.customer_id, invoice.amount, invoice.status, invoice.date],
-        ),
-    ),
-  );
+    const insertedInvoices = await Promise.all(
+      invoices.map(
+        (invoice) =>
+          client.query(
+            'INSERT INTO invoices (customer_id, amount, status, date) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO NOTHING;',
+            [invoice.customer_id, invoice.amount, invoice.status, invoice.date],
+          ),
+      ),
+    );
 
-  return insertedInvoices;
+    return insertedInvoices;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    throw error;
+  } finally {
+    client.release();
+  }
 }
 
 async function seedRevenue() {
-  await pool.query(`
+  const client = await pool.connect();
+  try {
+    await client.query(`
     CREATE TABLE IF NOT EXISTS revenue (
       month VARCHAR(4) NOT NULL UNIQUE,
       revenue INT NOT NULL
     );
   `);
 
-  const insertedRevenues = await Promise.all(
-    revenue.map(
-      (rev) =>
-        pool.query(
-          'INSERT INTO revenue (month, revenue) VALUES ($1, $2) ON CONFLICT (month) DO NOTHING;',
-          [rev.month, rev.revenue],
-        ),
-    ),
-  );
+    const insertedRevenues = await Promise.all(
+      revenue.map(
+        (rev) =>
+          client.query(
+            'INSERT INTO revenue (month, revenue) VALUES ($1, $2) ON CONFLICT (month) DO NOTHING;',
+            [rev.month, rev.revenue],
+          ),
+      ),
+    );
 
-  return insertedRevenues;
+    return insertedRevenues;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    throw error;
+  } finally {
+    client.release();
+  }
 }
 
 export async function GET() {
@@ -118,16 +153,14 @@ export async function GET() {
   //     output,
   // });
   try {
-    await pool.query('BEGIN');
     await seedUser();
     await seedCustomer();
     await seedInvoice();
     await seedRevenue();
-    await pool.query('COMMIT');
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
-    await pool.query('ROLLBACK');
+    // await pool.query('ROLLBACK');
     return Response.json({ error }, { status: 500 });
   }
 }
