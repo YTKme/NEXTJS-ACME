5d6d2cf26bfa65562c29d98b05706aa5e086740b
Yan Kuang <YTKme@Outlook.com>
2024-09-20 00:02:12 -0700

    Update dashboard

--- alpha/app/dashboard/page.tsx
+++ alpha/app/dashboard/page.tsx
@@ -5,14 +5,52 @@
 
 import type { Metadata } from 'next';
 
+import { Card } from '@/app/dashboard/card';
+import LatestInvoices from '@/app/dashboard/latestinvoice';
+import RevenueChart from '@/app/dashboard/revenuechart';
+
+import {
+  fetchCardData,
+  fetchLatestInvoices,
+  fetchRevenue,
+} from '@/app/library/data';
+
+import { lusitana } from '@/app/interface/font';
 
 export const metadata: Metadata = {
   title: 'NEXT.JS ACME | Dashboard',
   description: 'NEXT.JS ACME | Dashboard',
 }
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
-    <p>Dashboard Page</p>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
   );
 }
