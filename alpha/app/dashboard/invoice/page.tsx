a47d4644cbbe5efa8d06b72c60d49c87128ada07
Yan Kuang <YTKme@Outlook.com>
2024-10-14 18:26:44 -0700

    Mark: Update alpha to chapter 11

--- alpha/app/dashboard/invoice/page.tsx
+++ alpha/app/dashboard/invoice/page.tsx
@@ -2,17 +2,53 @@
  * Invoice Page
  */
 
-
 import type { Metadata } from 'next';
+import { Suspense } from 'react';
 
+// import Pagination from '@/app/ui/invoices/pagination';
+import { CreateInvoice } from '@/app/interface/invoice/button';
+import Pagination from '@/app/interface/invoice/pagination';
+import Table from '@/app/interface/invoice/table';
+import Search from '@/app/interface/search';
+import { InvoicesTableSkeleton } from '@/app/interface/skeleton';
+
+import { lusitana } from '@/app/interface/font';
+
+import { fetchInvoicesPages } from '@/app/library/data';
 
 export const metadata: Metadata = {
   title: 'NEXT.JS ACME | Invoice',
   description: 'NEXT.JS ACME | Invoice',
 }
 
-export default function InvoicePage() {
+export default async function InvoicePage({
+  searchParameter,
+}: {
+  searchParameter?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParameter?.query || '';
+  const currentPage = Number(searchParameter?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
-    <p>Invoice Page</p>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoice</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search Invoice..." />
+        <CreateInvoice />
+      </div>
+       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
   );
 }
